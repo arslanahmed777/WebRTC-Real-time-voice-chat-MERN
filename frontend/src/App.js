77d8a5ba43572a70e900ff951651efc3a4536e90
Arslan Ahmed <ashi3610@gmail.com>
2022-04-22 11:23:55 +0500

    add src folder, package.json and other files

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Home from './pages/Home/Home';
+import Navigation from './components/shared/Navigation/Navigation';
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact >
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
