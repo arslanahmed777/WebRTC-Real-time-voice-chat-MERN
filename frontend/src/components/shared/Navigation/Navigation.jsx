ee831304d76304e3be703642061afc4958dc34e5
Arslan Ahmed <ashi3610@gmail.com>
2022-04-22 14:30:11 +0500

    change frontend

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import styles from "./Navigation.module.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css';
+
 const Navigation = () => {
     const brandStyle = {
         color: '#fff',
@@ -10,17 +11,19 @@ const Navigation = () => {
         display: 'flex',
         alignItems: 'center',
     };
+
     const logoText = {
         marginLeft: '10px',
     };
+
     return (
         <nav className={`${styles.navbar} container`}>
             <Link style={brandStyle} to="/">
-                <img src="./images/logo.png" alt="logo" />
+                <img src="/images/logo.png" alt="logo" />
                 <span style={logoText}>Codershouse</span>
             </Link>
-        </nav >
-    )
-}
+        </nav>
+    );
+};
 
-export default Navigation
+export default Navigation;
