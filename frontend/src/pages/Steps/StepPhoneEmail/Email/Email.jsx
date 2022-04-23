23483f1dd38a768109f7db504554e1d539b717f4
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-04-23 15:57:25 +0500

    make steps and add protected routes

---
+++ frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../../../../components/shared/Card/Card'
+import Button from '../../../../components/shared/Button/Button'
+
+const Email = () => {
+    return (
+        <Card title="Enter your email" icon="email-emoji">
+            <div>
+                <Button text="Let's Go" />
+            </div>
+        </Card>
+    )
+}
+
+export default Email
---
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../../../../components/shared/Card/Card'
+import Button from '../../../../components/shared/Button/Button'
+
+const Phone = () => {
+    return (
+        <Card title="Enter your phone number" icon="phone">
+            <div>
+                <Button text="Let's Go" />
+            </div>
+        </Card>
+    )
+}
+
+export default Phone
