9a128648e66d390549cf742fe2f4fd1a50284e3e
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-04-24 08:04:41 +0500

    implement twillio integeration and send and verify OTP

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -11,8 +11,33 @@ class AuthController {
         const otp = await otpService.generateOtp();
         const data = `${phone}.${otp}.${expires}`;
         const hash = hashService.hashOtp(data);
-        res.json({ hash })
+        try {
+            //await otpService.sendBySms(phone, otp);
+            res.json({ hash: `${hash}.${expires}`, phone, otp, });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'message sending failed' });
+        }
+    }
+    async verifyOtp(req, res) {
+        const { otp, hash, phone } = req.body;
+        if (!otp || !hash || !phone) {
+            res.status(400).json({ message: 'All fields are required!' });
+        }
+        const [hashedOtp, expires] = hash.split('.');
+        if (Date.now() > +expires) {
+            res.status(400).json({ message: 'OTP expired!' });
+        }
 
+        const data = `${phone}.${otp}.${expires}`;
+
+        const isValid = otpService.verifyOtp(hashedOtp, data);
+        if (!isValid) {
+            res.status(400).json({ message: 'Invalid OTP' });
+        }
+        else {
+            res.status(200).json({ message: 'SUCCESS' });
+        }
     }
 }
 module.exports = new AuthController();
