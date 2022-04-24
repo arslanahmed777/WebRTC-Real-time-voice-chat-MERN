5ed02234995f4f77e8ee084db8c820ba339fc970
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-04-24 13:56:09 +0500

    add cors, axios and make send otp request

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const router = require('./routes');
 const DbConnect = require('./database');
+const cors = require('cors');
+
 
 const app = express()
 const PORT = process.env.PORT || 5000
 DbConnect();
+
+const corsOption = { origin: ['http://localhost:3000'], };
+app.use(cors(corsOption));
+
+
+app.use(express.json());
+app.use(router)
 app.get('/', (req, res) => {
     res.send("Hello Worlcd")
 })
-app.use(express.json());
-app.use(router)
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -3,14 +3,24 @@ import Card from '../../../../components/shared/Card/Card'
 import Button from '../../../../components/shared/Button/Button'
 import TextInput from '../../../../components/shared/TextInput/TextInput'
 import styles from '../StepPhoneEmail.module.css';
+import { sendOtp } from '../../../../http';
 const Phone = ({ onNext }) => {
     const [phoneNumber, setPhoneNumber] = useState('');
+    const Submit = async () => {
+        try {
+            const response = await sendOtp({ phone: phoneNumber })
+            console.log("response", response);
+        } catch (error) {
+            console.error("error", error);
+        }
+        //onNext()
+    }
     return (
         <Card title="Enter your phone number" icon="phone">
             <TextInput value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
             <div>
                 <div className={styles.actionButtonWrap}>
-                    <Button text="Next" onClick={onNext} />
+                    <Button text="Next" onClick={Submit} />
                 </div>
                 <p className={styles.bottomParagraph}>
                     By entering your number, you’re agreeing to our Terms of
