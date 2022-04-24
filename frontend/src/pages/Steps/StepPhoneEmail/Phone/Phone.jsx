401cd48d94238f2d99d7537027575985fc94d317
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-04-24 14:21:25 +0500

    add redus, redu-toolkit make store and reducer and save phone and hash in redux

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -4,12 +4,17 @@ import Button from '../../../../components/shared/Button/Button'
 import TextInput from '../../../../components/shared/TextInput/TextInput'
 import styles from '../StepPhoneEmail.module.css';
 import { sendOtp } from '../../../../http';
+import { useDispatch } from 'react-redux';
+import { setOtp } from '../../../../store/authSlice';
 const Phone = ({ onNext }) => {
     const [phoneNumber, setPhoneNumber] = useState('');
+    const dispatch = useDispatch();
     const Submit = async () => {
         try {
-            const response = await sendOtp({ phone: phoneNumber })
-            console.log("response", response);
+            const { data } = await sendOtp({ phone: phoneNumber })
+            console.log("response", data);
+            dispatch(setOtp({ phone: data.phone, hash: data.hash }));
+            onNext();
         } catch (error) {
             console.error("error", error);
         }
