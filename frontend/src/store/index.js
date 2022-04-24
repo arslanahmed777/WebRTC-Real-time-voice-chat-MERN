154b428de271f3483ca0a2ba152d3ff36493bd86
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-04-24 17:01:06 +0500

    make step avatar and name and add activate slice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import auth from './authSlice';
+import activate from './activateSlice';
 export const store = configureStore({
-    reducer: { auth },
+    reducer: { auth, activate },
 })
