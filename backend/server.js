71bf40c516ec99aa6eadf97c3c47a15426b071d7
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2022-04-25 08:34:31 +0500

    add cookie-parser and jimp   make auth middleware and controller

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,18 @@ const express = require('express')
 const router = require('./routes');
 const DbConnect = require('./database');
 const cors = require('cors');
-
+const cookieParser = require('cookie-parser');
 
 const app = express()
 const PORT = process.env.PORT || 5000
 DbConnect();
 
+app.use(cookieParser());
 const corsOption = { origin: ['http://localhost:3000'], credentials: true, };
 app.use(cors(corsOption));
 
 
-app.use(express.json());
+app.use(express.json({ limit: '8mb' }));
 app.use(router)
 app.get('/', (req, res) => {
     res.send("Hello Worlcd")
