14d5fbca65897f25407cf8bb15202c9db57ec00b
Arslan Ahmed <ashi3610@gmail.com>
2022-04-26 11:45:50 +0500

    solve react cleanup warning

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../../../components/shared/Card/Card';
 import Button from '../../../components/shared/Button/Button';
 import styles from './StepAvatar.module.css';
@@ -13,6 +13,7 @@ const StepAvatar = ({ onNext }) => {
     const { name, avatar } = useSelector((state) => state.activate);
     const [image, setImage] = useState('/images/monkey-avatar.png');
     const [loading, setLoading] = useState(false);
+    const [unMounted, setUnMounted] = useState(false);
     const captureImage = (e) => {
         const file = e.target.files[0];
         const reader = new FileReader();
@@ -28,7 +29,9 @@ const StepAvatar = ({ onNext }) => {
         try {
             const { data } = await activate({ name, avatar });
             if (data.auth) {
-                dispatch(setAuth(data));
+                if (!unMounted) {
+                    dispatch(setAuth(data));
+                }
             }
         } catch (err) {
             console.log(err);
@@ -36,6 +39,11 @@ const StepAvatar = ({ onNext }) => {
             setLoading(false);
         }
     }
+    useEffect(() => {
+        return () => {
+            setUnMounted(true);
+        }
+    }, [])
 
     if (loading) return <Loader message="Activation in progress..." />;
     return (
